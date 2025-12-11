dab72c08f8e6ef3d8a14bad6532e9429f20c14f7
gnihanyazici <yazicigl@gmail.com>
2025-12-11 19:59:09 +0300

    Update eslint.config.js

--- goit-react-hw-01/eslint.config.js
+++ goit-react-hw-01/eslint.config.js
@@ -23,7 +23,11 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+     "react/prop-types": 0,
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
     },
   },
 ])
